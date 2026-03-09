6f67eb60c22526e2d4fcacb41d6929bc19aba49d
Alexander5265 <104996703+Alexander5265@users.noreply.github.com>
2026-03-09 20:50:53 +0500

    loh

---
+++ 2_2/2_2/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+// функция перевода двоичного числа в десятичное
+let binaryToDecimal (binary: string) =
+    Convert.ToInt32(binary, 2)
+
+// функция для вычисления суммы с помощью List.fold
+let sumBinaryNumbers (numbers: string list) =
+    List.fold (fun sum element ->
+        sum + binaryToDecimal element
+    ) 0 numbers
+
+
+[<EntryPoint>]
+let main argv =
+
+    printfn "Исходный двоичный список через пробел: "
+    let input1 = Console.ReadLine()
+    let list1 = input1.Split(' ') |> Array.toList
+        
+    let result = sumBinaryNumbers list1
+
+    printfn "Сумма = %d" result
+
+    0
