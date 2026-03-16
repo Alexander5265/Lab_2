a177d2a6ee779bda18100463b2fe6e0866435c00
Alexander5265 <104996703+Alexander5265@users.noreply.github.com>
2026-03-16 08:19:03 +0500

    Update Program.fs

--- 2_1/2_1/Program.fs
+++ 2_1/2_1/Program.fs
@@ -2,22 +2,26 @@ open System
 
 // Функция для вычисления произведения цифр числа
 let product (n: int) =
-    let rec prod x =
-        if x = 0 then 1
-        else (x % 10) * prod (x / 10)
-
     let a = abs n
     if a = 0 then 0
-    else prod a
+    else
+        let rec prod x =
+            if x = 0 then 1
+            else (x % 10) * prod (x / 10)
+        prod a
 
-// Функция, которая получает список произведений цифр
 let getProducts (numbers: int list) =
-    numbers |> List.map product
+    List.map product numbers
 
-printfn "Исходный список через пробел: "
-let input1 = Console.ReadLine()
-let list1 = input1.Split(' ') |> Array.toList |> List.map int
+[<EntryPoint>]
+let main argv =
 
-let result = getProducts list1
+    printfn "Исходный список через пробел: "
+    let input1 = Console.ReadLine()
+    let list1 = input1.Split(' ') |> Array.toList |> List.map int
+        
+    let result = getProducts list1
 
-printfn "%A" result
+    printfn "Результат: %A" result
+
+    0
